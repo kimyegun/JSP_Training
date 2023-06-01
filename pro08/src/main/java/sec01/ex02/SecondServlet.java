ef509eaef620a11b28b2fa7247aacb3a089445ee
kimyegun123 <kimyegun123@gmail.com>
2023-06-01 17:54:12 +0900

    pro08

---
+++ pro08/src/main/java/sec01/ex02/SecondServlet.java
@@ -0,0 +1,23 @@
+package sec01.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet("/second")
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.print("<html><body>");
+		out.print("refresh를 이용한 redirect 실습입니다.");
+		out.print("</body></html>");
+	}
+
+}
---
+++ pro08/src/main/java/sec04/ex02/FirstServlet.java
@@ -0,0 +1,24 @@
+package sec04.ex02;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		request.setAttribute("address", "서울시 성복구");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("second");
+		dispatcher.forward(request, response);
+	}
+
+}
